34b55f2f4ee000605718d448c7901f5909203897
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-09-15 13:12:56 -0400

    created function for when created gif button is clicked to run ajax request,created funtion to append gifs to page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,9 @@
 var tvShows =['Chappelles Show','TWD','The Office','Hero Academia'];
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=2j1brct1vXBta8bqyeIKM7ERP2kia1nO"
+
+
+
+
 
 function renderButtons(){
     $('#buttons-view').empty();
@@ -17,4 +22,31 @@ function renderButtons(){
         tvShows.push(tvShow);
         renderButtons();
     })
+$(document).on('click', '.gif-button',function(){
+    console.log("hello");
+    var gif = $(this).attr('data-name');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ gif +"&api_key=2j1brct1vXBta8bqyeIKM7ERP2kia1nO&limit=10"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        // console.log(queryURL);
+        console.log(response);
+    
+        var results = response.data;
+        for (var a = 0; a < results.length; a++){
+            var gifDiv =$('<div>');
+            gifDiv.addClass('gifs');
+            var p = $('<p>').text("Rating: " + results[a].rating);
+            var gifImage = $('<img>');            
+            gifImage.attr('src', results[a].images.fixed_height_still.url);
+            gifDiv.append(p);
+            gifDiv.append(gifImage);
+            $('#gifsHere').prepend(gifDiv);
+            
+        }
+        
+    })
+})
     renderButtons();
